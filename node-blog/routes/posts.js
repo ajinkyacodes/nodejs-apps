fd96d20ecff2e5b7bdf0dd8415b15a607ada648d
AJINKYA <ajinkyacodes@gmail.com>
2023-12-14 22:03:22 +0530

    Created comments functionality on blog post

--- node-blog/routes/posts.js
+++ node-blog/routes/posts.js
@@ -72,4 +72,62 @@ router.post('/add', upload.single('mainimage'), function(req, res, next) {
 	}
 });
 
+
+router.post('/addcomment', function(req, res, next) {
+	// Get Form Values
+	var name = req.body.name;
+	var email= req.body.email;
+	var body = req.body.body;
+	var postid = req.body.postid;
+	var commentdate = new Date();
+  
+	// Form Validation
+	req.checkBody('name','Name field is required').notEmpty();
+	req.checkBody('email', 'Email field is required but it will never displayed').notEmpty();
+	req.checkBody('email', 'Email is not formatted properly').isEmail();
+	req.checkBody('body','Body field is required').notEmpty();
+
+	// Check Errors
+	var errors = req.validationErrors();
+  
+	  if(errors){
+		  var posts = db.get('posts');
+		  posts.findOne(postid, function(err, post){
+			res.render('show', {
+				"errors": errors,
+				"post": post
+			});
+		  });
+		  res.render('show',{
+			  "errors": errors
+		  });
+	  } else {
+		  var comment = {
+			"name": name,
+			"email": email,
+			"body": body,
+			"commentdate": commentdate
+		  }
+
+		  var posts = db.get('posts');
+		  posts.update({
+			"_id": postid
+		  }, {
+			$push: {
+				"comments": comment
+			}
+		  }, function(err, doc) {
+			if(err){
+				throw err;
+			} else {
+				req.flash('success', 'Comment added');
+				res.location('/posts/show/'+postid);
+				res.redirect('/posts/show/'+postid);
+			}
+		  })
+	  }
+  });
+
+
+
 module.exports = router;
